a2081b4c35f41f9dcdf316cb227f94ea399fdddf
Jeremy Kenyon <lanthos@gmail.com>
2015-10-31 19:11:26 -0700

    Story bits updated
    Removed the formatting, upon testing it more it was a cool idea but
    looks horrible.

--- js/win.js
+++ js/win.js
@@ -1,10 +1,18 @@
 /* global Controller, HUD, Helpers */
 
 var winSound = new Audio('sounds/win_sound.mp3');
-var storyBits = {2:'Walls and haystacks.<br></<br> That\'s all the <b>Storied</b> history of codes and <span class="strike">secrets</span> have amounted to.<br></<br> Just walls and haystacks.', 
-5:'Rivers of redacted pages, ink upon ink, <span class="red">blood</span> after <span class="red">blood</span>.<br></<br> We\'ve always known of the monster under the bed.', 
-6:'<span class="strike">Always</span> seen through the <span class="under">lies told to comfort</span>, told to <span class="strike">Protect</span>. But the lies hide more than the monster.<br></br> They keep us from wondering what it\'s hiding <i>from</i>.',
-11:'Information reigns over an empty <span class="strike">Kingdom</span>.<br></br>The subversive truth, our rumpelstiltskin - <br></br>To learn its name is to <b>steal away</b> its <span class="under">power<span>.'}
+var storyBits = {5:"Walls and haystacks.<br></br> That's all the Storied history of codes and secrets have amounted to.<br></br> Just walls and haystacks.", 
+10:"Rivers of redacted pages, ink upon ink, blood after blood.<br></br>We've always known of the monster under the bed.", 
+15:"Always seen through the lies told to comfort, told to Protect. But the lies hide more than the monster.<br></br> They keep us from wondering what it's hiding <i>from</i>.",
+20:"Information reigns over an empty Kingdom.<br></br>The subversive truth, our rumpelstiltskin - <br></br>To learn its name is to steal away its power.",
+25:"For all the walls, all the haystacks, the whole of it remains a house of Cards for one simple reason: <br></br>A lock is built to be opened.",
+30:"Boxes within boxes; secrets within secrets;<br><br>what you don’t know can’t haunt you<br></br>right?",
+35:"If you give a mouse a cookie, it will eat for a day.<br></br>It will learn where the cookies are kept.",
+40:"Scratching beneath the floorboards, burrowing in the walls,<br></br>traps and cats can kill, but no cookie is safe forever.",
+45:"We are all Americans now.<br></br>We are all on camera, in writing, stored and logged, classified and eyes only.",
+50:"We are all suspected, all traitors in the making, all criminals and revolutionaries.<br></br>We are all Americans now.",
+55:"May the right hand never know who the left is hurting.<br></br>The magician's assistant is in on the trick;<br></br>dead rabbits are cheaper than magic."
+};
 
 function WinScene(container){
   'use strict';
