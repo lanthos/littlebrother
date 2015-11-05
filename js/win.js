5a81c2fe63a3d2d248fd791fade8168ba53f525e
Jeremy Kenyon <lanthos@gmail.com>
2015-11-04 20:14:25 -0800

    Story beats every level!

--- js/win.js
+++ js/win.js
@@ -1,17 +1,17 @@
 /* global Controller, HUD, Helpers */
 
 var winSound = new Audio('sounds/win_sound'+ Controller.audioFormat);
-var storyBits = {5:"Walls and haystacks.<br></br> That's all the Storied history of codes and secrets have amounted to.<br></br> Just walls and haystacks.", 
-10:"Rivers of redacted pages, ink upon ink, blood after blood.<br></br>We've always known of the monster under the bed.", 
-15:"Always seen through the lies told to comfort, told to Protect. But the lies hide more than the monster.<br></br> They keep us from wondering what it's hiding <i>from</i>.",
-20:"Information reigns over an empty Kingdom.<br></br>The subversive truth, our rumpelstiltskin - <br></br>To learn its name is to steal away its power.",
-25:"For all the walls, all the haystacks, the whole of it remains a house of Cards for one simple reason: <br></br>A lock is built to be opened.",
-30:"Boxes within boxes; secrets within secrets;<br><br>what you don’t know can’t haunt you<br></br>right?",
-35:"If you give a mouse a cookie, it will eat for a day.<br></br>It will learn where the cookies are kept.",
-40:"Scratching beneath the floorboards, burrowing in the walls,<br></br>traps and cats can kill, but no cookie is safe forever.",
-45:"We are all Americans now.<br></br>We are all on camera, in writing, stored and logged, classified and eyes only.",
-50:"We are all suspected, all traitors in the making, all criminals and revolutionaries.<br></br>We are all Americans now.",
-55:"May the right hand never know who the left is hurting.<br></br>The magician's assistant is in on the trick;<br></br>dead rabbits are cheaper than magic."
+var storyBits = {1:"Walls and haystacks.<br></br> That's all the Storied history of codes and secrets have amounted to.<br></br> Just walls and haystacks.", 
+2:"Rivers of redacted pages, ink upon ink, blood after blood.<br></br>We've always known of the monster under the bed.", 
+3:"Always seen through the lies told to comfort, told to Protect. But the lies hide more than the monster.<br></br> They keep us from wondering what it's hiding <i>from</i>.",
+4:"Information reigns over an empty Kingdom.<br></br>The subversive truth, our rumpelstiltskin - <br></br>To learn its name is to steal away its power.",
+5:"For all the walls, all the haystacks, the whole of it remains a house of Cards for one simple reason: <br></br>A lock is built to be opened.",
+6:"Boxes within boxes; secrets within secrets;<br><br>what you don’t know can’t haunt you<br></br>right?",
+7:"If you give a mouse a cookie, it will eat for a day.<br></br>It will learn where the cookies are kept.",
+8:"Scratching beneath the floorboards, burrowing in the walls,<br></br>traps and cats can kill, but no cookie is safe forever.",
+9:"We are all Americans now.<br></br>We are all on camera, in writing, stored and logged, classified and eyes only.",
+10:"We are all suspected, all traitors in the making, all criminals and revolutionaries.<br></br>We are all Americans now.",
+11:"May the right hand never know who the left is hurting.<br></br>The magician's assistant is in on the trick;<br></br>dead rabbits are cheaper than magic."
 };
 
 function WinScene(container){
